a34f442673a1e7ebd33ac79d4787687c67ad8f84
Bhavya Goel <bgoel4132@gmail.com>
2025-01-07 23:24:29 +0530

    Fix: Fix cors for the service

--- backend/main.py
+++ backend/main.py
@@ -23,7 +23,7 @@ app = FastAPI(title="Instagram Data Insights API", description="API for fetching
 #Allow access from local frontend server
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://destiny-supermind-frontend.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
