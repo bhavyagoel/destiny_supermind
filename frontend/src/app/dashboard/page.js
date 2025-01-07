5356b758d843f6758a4b5132be654d607eded660
Kaustubh251002 <kmishra2510@gmail.com>
2025-01-07 11:59:32 +0530

    Feat: updated charts, timestamp grouping, performance optimizations

--- frontend/src/app/dashboard/page.js
+++ frontend/src/app/dashboard/page.js
@@ -3,7 +3,7 @@
 import Navbar from "../../components/Dashboard/Navbar";
 import PerformanceOverview from "../../components/Dashboard/PerformanceOverview";
 import AIChat from "../../components/Dashboard/AIChat";
-import MetricCard from "../../components/Dashboard/MetricCard";
+import tempData from "D:/stuff/dev/ig-profile-data/output.json" assert {type: "json"};
 
 const Dashboard = () => {
   return (
@@ -12,13 +12,7 @@ const Dashboard = () => {
       <main className="pt-20 px-4 lg:px-8 h-screen flex flex-col lg:flex-row gap-6">
         {/* Left Section */}
         <div className="lg:w-3/4 flex flex-col gap-6 h-full">
-          <PerformanceOverview />
-          <div className="flex space-x-6">
-            <MetricCard title="Followers" value="10k" color="text-blue-600" />
-            <MetricCard title="Likes" value="12.5k" color="text-green-600" />
-            <MetricCard title="Comments" value="2.3k" color="text-yellow-600" />
-            <MetricCard title="Shares" value="1.5k" color="text-red-600" />
-          </div>
+          <PerformanceOverview data={tempData}/>
         </div>
 
         {/* Right Section */}
